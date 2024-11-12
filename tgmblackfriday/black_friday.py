79b8cf783a8e928bddfea49f9d81661cf99a0059
Vojtech Jindra <jindravo@gmail.com>
2024-11-12 22:54:35 +0100

    add feature variable preprocessing options

--- tgmblackfriday/black_friday.py
+++ tgmblackfriday/black_friday.py
@@ -5,18 +5,80 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
 from google.cloud import storage
 
+from enum import Enum
+
+
+class EncodingType(Enum):
+    BINARY = 0
+    ORDINAL = 1
+    ONE_HOT = 2
+
+
+class ProductCategoriesEncoder:
+    product_category_cols: list[str] = ['Product_Category_1', 'Product_Category_2', 'Product_Category_3']
+
+
+    def __init__(self, encoding: EncodingType, product_category_cols: list[str] = product_category_cols):
+        self.encoding = encoding
+        self.product_category_cols = product_category_cols
+
+
+    def fit(self, df: pd.DataFrame) -> None:
+        categories = set()
+        for product_category in self.product_category_cols:
+            categories |= set(df[product_category].dropna().unique())
+
+        categories = sorted(list(categories))
+
+        self._product_categories = categories
+
+
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        if self.encoding.value == EncodingType.ONE_HOT.value:
+            df_products = df.drop_duplicates(subset=['Product_ID'])
+            df_products = df_products.set_index('Product_ID')
+            df_products = df_products[self.product_category_cols]
+
+            df_products_encoded = df_products[self.product_category_cols].apply(self._encode_product_row_one_hot, axis=1)
+            df_products_encoded = df_products_encoded.loc[:, df_products_encoded.columns[1:]]
+
+            df = df.drop(columns=self.product_category_cols).merge(df_products_encoded, left_on='Product_ID', right_index=True)
+        elif self.encoding.value == EncodingType.ORDINAL.value:
+            # Product categories already preserve ordinal relationship.
+            # Keeping this here for clarity.
+            ...
+
+        return df
+
+
+    def _encode_product_row_one_hot(self, row: pd.Series) -> pd.Series:
+        product_categories = row.dropna().values
+        encoded_row = pd.Series({
+            f'Product_Category_{category}': 1 if category in product_categories else 0 for category in self._product_categories
+        })
+
+        return encoded_row
+
 
 class BlackFridayDataset:
-    _age_encoder: OrdinalEncoder | None = None
-    _city_category_encoder: OneHotEncoder | None = None
-    _occupation_encoder: OneHotEncoder | None = None
-    _stay_in_current_city_years_encoder: OrdinalEncoder | None = None
-    _product_categories: list[int] | None = None
+    _age_encoder: OrdinalEncoder | OneHotEncoder | None = None
+    _city_category_encoder: OrdinalEncoder | OneHotEncoder | None = None
+    _occupation_encoder: OrdinalEncoder | OneHotEncoder | None = None
+    _stay_in_current_city_years_encoder: OrdinalEncoder | OneHotEncoder | None = None
+    _product_category_encoder: ProductCategoriesEncoder | None = None
+
+    demographic_cols: list[str] = [
+        'Gender', 'Age', 'City_Category', 'Occupation',
+        'Marital_Status', 'Stay_In_Current_City_Years'
+    ]
+
+    product_category_cols: list[str] = ['Product_Category_1', 'Product_Category_2', 'Product_Category_3']
 
 
     def __init__(self, path: str, test_path: str = None):
         if path.startswith('gs://'):
             self.gcs_client = storage.Client.create_anonymous_client()
+
             self.df = pd.read_csv(path)
             self.df_test = pd.read_csv(test_path) if test_path else None
         else:
@@ -24,7 +86,7 @@ class BlackFridayDataset:
             self.df_test = self._load_data_file(test_path) if test_path else None
 
 
-    def preprocess_dfs(self, return_res: bool = True) -> None | tuple[pd.DataFrame, pd.DataFrame | None]:
+    def preprocess_dfs(self, encodings: dict[str, EncodingType], return_res: bool = True) -> None | tuple[pd.DataFrame, pd.DataFrame | None]:
         """Processes the features in the loaded dataframes to be ready to be used
         in a model. Specific processing steps for each feature are further
         explained in the relevant functions
@@ -37,22 +99,22 @@ class BlackFridayDataset:
                 is True, returns the processed dataframe(s), depending on whether
                 the test dataset was loaded
         """
-        process_feature_funcs = [
-            self._process_gender_col, self._process_age_col, self._process_city_category_col,
-            self._process_stay_in_current_city_years_col, self._process_occupation_col,
-            self._process_product_category_cols
-        ]
+
+
+        cols_to_drop = set(self.demographic_cols + ['Product_Category']) - set(encodings.keys())
+        self.df = self.df.drop(columns=cols_to_drop)
+        self.df_test = self.df_test.drop(columns=cols_to_drop) if self.df_test is not None else None
 
         self.df_encoded = self.df.copy()
         self.df_test_encoded = self.df_test.copy() if self.df_test is not None else None
         self.df_encoded = self._preprocess_df(
             self.df_encoded,
-            process_feature_funcs
+            encodings
         )
 
         self.df_test_encoded = self._preprocess_df(
             self.df_test_encoded,
-            process_feature_funcs
+            encodings
         ) if self.df_test_encoded is not None else None
 
         return (self.df_encoded, self.df_test_encoded) if return_res else None
@@ -154,7 +216,7 @@ class BlackFridayDataset:
         return (self.X_train, self.y_train, self.X_val, self.y_val, self.X_test) if return_res else None
 
 
-    def _preprocess_df(self, df: pd.DataFrame, process_feature_funcs: list[callable]) -> pd.DataFrame:
+    def _preprocess_df(self, df: pd.DataFrame, encodings: dict[str, EncodingType]) -> pd.DataFrame:
         """Processes the features in the loaded dataframe to be ready to be used
         in a model. Specific processing steps for each feature are further
         explained in the relevant functions
@@ -167,8 +229,19 @@ class BlackFridayDataset:
         Returns:
             pd.DataFrame: The processed dataframe
         """
-        for func in process_feature_funcs:
-            df = func(df)
+        col_func_mapping = {
+            'Gender': self._process_gender_col,
+            'Age': self._process_age_col,
+            'City_Category': self._process_city_category_col,
+            'Stay_In_Current_City_Years': self._process_stay_in_current_city_years_col,
+            'Occupation': self._process_occupation_col,
+            'Product_Category': self._process_product_category_cols
+        }
+
+        for col, encoding in encodings.items():
+            if col in col_func_mapping.keys():
+                col_func = col_func_mapping[col]
+                df = col_func(df, encoding)
 
         return df
 
@@ -193,7 +266,7 @@ class BlackFridayDataset:
             raise ValueError(f"Incorrect path to the dataset: '{path}'")
 
 
-    def _process_gender_col(self, df: pd.DataFrame) -> pd.DataFrame:
+    def _process_gender_col(self, df: pd.DataFrame, encoding: EncodingType) -> pd.DataFrame:
         """Maps the 'F' and 'M' strings in the 'Gender' column of the
         `df` dataframe to 0 and 1 respectively
 
@@ -209,8 +282,44 @@ class BlackFridayDataset:
 
         return df
 
+    def _prepare_col_encoder(
+        self,
+        df: pd.DataFrame,
+        encoder_type: EncodingType,
+        col: str
+    ) -> OneHotEncoder | OrdinalEncoder:
+        categories = sorted(df[col].unique())
 
-    def _process_age_col(self, df: pd.DataFrame) -> pd.DataFrame:
+        if encoder_type.value == EncodingType.ORDINAL.value:
+            encoder = OrdinalEncoder(categories=[categories], dtype=int)
+            encoder.fit(df[[col]])
+
+        elif encoder_type.value == EncodingType.ONE_HOT.value:
+            encoder = OneHotEncoder(drop='first', categories=[categories], dtype=int)
+            encoder.fit(df[[col]])
+
+        else:
+            raise ValueError(f"Invalid encoding type: {encoder_type}")
+
+        return encoder
+
+
+    def _encode_col(self, df: pd.DataFrame, encoder: OneHotEncoder | OrdinalEncoder, col: str) -> pd.DataFrame:
+        if isinstance(encoder, OrdinalEncoder):
+            df[col] = encoder.transform(df[[col]])
+
+        elif isinstance(encoder, OneHotEncoder):
+            col_names = [f'{col}_{category}' for category in encoder.categories[0][1:]]
+            df[col_names] = encoder.transform(df[[col]]).toarray()
+            df = df.drop(columns=[col])
+
+        else:
+            raise ValueError(f"Invalid encoder type: {type(encoder)}")
+
+        return df
+
+
+    def _process_age_col(self, df: pd.DataFrame, encoding: EncodingType) -> pd.DataFrame:
         """Encodes the `Age` column of the `df` dataframe to respective
         ordinal integer values from 0 to N where N is the number of
         different age categories in the `df` dataframe
@@ -224,16 +333,12 @@ class BlackFridayDataset:
                 to ordinal integer values
         """
         if not self._age_encoder:
-            age_categories = sorted(df['Age'].unique())
-            self._age_encoder = OrdinalEncoder(categories=[age_categories], dtype=int)
-            self._age_encoder.fit(df[['Age']])
+            self._age_encoder = self._prepare_col_encoder(df, encoding, 'Age')
 
-        df['Age'] = self._age_encoder.transform(df[['Age']])
-
-        return df
+        return self._encode_col(df, self._age_encoder, 'Age')
 
 
-    def _process_city_category_col(self, df: pd.DataFrame) -> pd.DataFrame:
+    def _process_city_category_col(self, df: pd.DataFrame, encoding: EncodingType) -> pd.DataFrame:
         """One-hot encodes the `City_Category` column of the `df` dataframe.
         The original `City_Category` column is dropped in favour of its encoded
         representation in N-1 new columns where N is the number of different city
@@ -250,18 +355,12 @@ class BlackFridayDataset:
                 The output column names are `City_Category_1`, `City_Category_2`, etc
         """
         if not self._city_category_encoder:
-            city_categories = sorted(df['City_Category'].unique())
-            self._city_category_encoder = OneHotEncoder(drop='first', categories=[city_categories], dtype=int)
-            self._city_category_encoder.fit(df[['City_Category']])
+            self._city_category_encoder = self._prepare_col_encoder(df, encoding, 'City_Category')
 
-        col_names = [f'City_Category_{category}' for category in self._city_category_encoder.categories[0][1:]]
-        df[col_names] = self._city_category_encoder.transform(df[['City_Category']]).toarray()
-        df = df.drop(columns=['City_Category'])
-
-        return df
+        return self._encode_col(df, self._city_category_encoder, 'City_Category')
 
 
-    def _process_stay_in_current_city_years_col(self, df: pd.DataFrame) -> pd.DataFrame:
+    def _process_stay_in_current_city_years_col(self, df: pd.DataFrame, encoding: EncodingType) -> pd.DataFrame:
         """Encodes the `Stay_In_Current_City_Years` column of the `df` dataframe to
         respective ordinal integer values from 0 to N where N is the number of
         different categories in the column
@@ -275,16 +374,11 @@ class BlackFridayDataset:
                 values mapped to ordinal integer values
         """
         if not self._stay_in_current_city_years_encoder:
-            stay_in_current_city_years_categories = sorted(df['Stay_In_Current_City_Years'].unique())
-            self._stay_in_current_city_years_encoder = OrdinalEncoder(categories=[stay_in_current_city_years_categories], dtype=int)
-            self._stay_in_current_city_years_encoder.fit(df[['Stay_In_Current_City_Years']])
+            self._stay_in_current_city_years_encoder = self._prepare_col_encoder(df, encoding, 'Stay_In_Current_City_Years')
 
-        df['Stay_In_Current_City_Years'] = self._stay_in_current_city_years_encoder.transform(df[['Stay_In_Current_City_Years']])
+        return self._encode_col(df, self._stay_in_current_city_years_encoder, 'Stay_In_Current_City_Years')
 
-        return df
-
-
-    def _process_occupation_col(self, df: pd.DataFrame) -> pd.DataFrame:
+    def _process_occupation_col(self, df: pd.DataFrame, encoding: EncodingType) -> pd.DataFrame:
         """One-hot encodes the `Occupation` column of the `df` dataframe.
         The original `Occupation` column is dropped in favour of its encoded
         representation in N-1 new columns where N is the number of different occupation
@@ -301,18 +395,12 @@ class BlackFridayDataset:
                 The output column names are `Occupation_1`, `Occupation_2`, etc
         """
         if not self._occupation_encoder:
-            occupation_categories = sorted(df['Occupation'].unique())
-            self._occupation_encoder = OneHotEncoder(drop='first', categories=[occupation_categories], dtype=int)
-            self._occupation_encoder.fit(df[['Occupation']])
+            self._occupation_encoder = self._prepare_col_encoder(df, encoding, 'Occupation')
 
-        col_names = [f'Occupation_{category}' for category in self._occupation_encoder.categories[0][1:]]
-        df[col_names] = self._occupation_encoder.transform(df[['Occupation']]).toarray()
-        df = df.drop(columns=['Occupation'])
-
-        return df
+        return self._encode_col(df, self._occupation_encoder, 'Occupation')
 
 
-    def _process_product_category_cols(self, df: pd.DataFrame) -> pd.DataFrame:
+    def _process_product_category_cols(self, df: pd.DataFrame, encoding: EncodingType) -> pd.DataFrame:
         """One-hot encodes the `Product_Category_*` columns of the `df` dataframe.
         The original `Product_Category_*` columns are dropped in favour of its encoded
         representation in N new columns where N is the number of different product
@@ -327,30 +415,10 @@ class BlackFridayDataset:
                 by N columns representing its one-hot encoded representation.
                 The output column names are `Product_Category_1`, `Product_Category_2`, etc
         """
-        def encode_product_row(row: pd.Series, categories: list[int]) -> pd.Series:
-            product_categories = row.dropna().values
-            encoded_row = pd.Series({f'Product_Category_{category}': 1 if category in product_categories else 0 for category in categories})
+        if not self._product_category_encoder:
+            self._product_category_encoder = ProductCategoriesEncoder(encoding=encoding, product_category_cols=self.product_category_cols)
+            self._product_category_encoder.fit(df)
 
-            return encoded_row
-
-        product_categories_cols = ['Product_Category_1', 'Product_Category_2', 'Product_Category_3']
-
-        if not self._product_categories:
-            categories = set()
-            for product_category in product_categories_cols:
-                categories |= set(df[product_category].dropna().unique())
-
-            categories = sorted(list(categories))
-
-            self._product_categories = categories
-
-        df_products = df.drop_duplicates(subset=['Product_ID'])
-        df_products = df_products.set_index('Product_ID')
-        df_products = df_products[product_categories_cols]
-
-        df_products_encoded = df_products[product_categories_cols].apply(encode_product_row, categories=self._product_categories, axis=1)
-        df_products_encoded = df_products_encoded.loc[:, df_products_encoded.columns[1:]]
-
-        df = df.drop(columns=product_categories_cols).merge(df_products_encoded, left_on='Product_ID', right_index=True)
+        df = self._product_category_encoder.transform(df)
 
         return df
